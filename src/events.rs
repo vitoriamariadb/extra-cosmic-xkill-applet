d4d5d6f2548246e7f325580d0aef336b695e69b1
Vitória Maria <vitoriamaria.sds@gmail.com>
2021-12-28 11:00:00 -0300

    feat: implementa captura eventos

---
+++ src/events.rs
@@ -0,0 +1,116 @@
+use anyhow::{Context, Result};
+use x11rb::connection::Connection;
+use x11rb::protocol::xproto::{
+    ButtonPressEvent, ConnectionExt, EventMask, GrabMode, ModMask, Window,
+};
+use x11rb::protocol::Event;
+use x11rb::rust_connection::RustConnection;
+
+pub struct EventHandler {
+    conn: RustConnection,
+    screen_num: usize,
+    active: bool,
+}
+
+impl EventHandler {
+    pub fn new(conn: RustConnection, screen_num: usize) -> Self {
+        Self {
+            conn,
+            screen_num,
+            active: false,
+        }
+    }
+
+    pub fn grab_pointer(&mut self) -> Result<()> {
+        let root = self.conn.setup().roots[self.screen_num].root;
+
+        self.conn
+            .grab_pointer(
+                false,
+                root,
+                EventMask::BUTTON_PRESS | EventMask::BUTTON_RELEASE,
+                GrabMode::ASYNC,
+                GrabMode::ASYNC,
+                x11rb::NONE,
+                x11rb::NONE,
+                x11rb::CURRENT_TIME,
+            )
+            .context("Falha ao capturar ponteiro")?
+            .reply()
+            .context("Falha ao obter resposta do grab")?;
+
+        self.active = true;
+        Ok(())
+    }
+
+    pub fn ungrab_pointer(&mut self) -> Result<()> {
+        self.conn
+            .ungrab_pointer(x11rb::CURRENT_TIME)
+            .context("Falha ao liberar ponteiro")?;
+        self.conn.flush().context("Falha ao flush")?;
+        self.active = false;
+        Ok(())
+    }
+
+    pub fn wait_for_window_click(&mut self) -> Result<Option<Window>> {
+        if !self.active {
+            self.grab_pointer()?;
+        }
+
+        loop {
+            let event = self
+                .conn
+                .wait_for_event()
+                .context("Falha ao aguardar evento")?;
+
+            match event {
+                Event::ButtonPress(btn_event) => {
+                    self.ungrab_pointer()?;
+                    return Ok(Some(self.get_top_level_window(btn_event.child)?));
+                }
+                Event::KeyPress(_) => {
+                    self.ungrab_pointer()?;
+                    return Ok(None);
+                }
+                _ => continue,
+            }
+        }
+    }
+
+    fn get_top_level_window(&self, mut window: Window) -> Result<Window> {
+        let root = self.conn.setup().roots[self.screen_num].root;
+
+        loop {
+            let tree = self
+                .conn
+                .query_tree(window)
+                .context("Falha ao consultar árvore")?
+                .reply()
+                .context("Falha ao obter resposta")?;
+
+            if tree.parent == root || tree.parent == 0 {
+                return Ok(window);
+            }
+
+            window = tree.parent;
+        }
+    }
+
+    pub fn register_hotkey(&self, modifiers: ModMask, keycode: u8) -> Result<()> {
+        let root = self.conn.setup().roots[self.screen_num].root;
+
+        self.conn
+            .grab_key(
+                false,
+                root,
+                modifiers,
+                keycode,
+                GrabMode::ASYNC,
+                GrabMode::ASYNC,
+            )
+            .context("Falha ao registrar hotkey")?;
+
+        self.conn.flush().context("Falha ao flush")?;
+        Ok(())
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,27 @@
+mod events;
 mod x11;
 
 use anyhow::Result;
+use events::EventHandler;
 use x11::X11Handler;
+use x11rb::rust_connection::RustConnection;
 
 fn main() -> Result<()> {
     println!("Extra Cosmic XKill Applet v0.1.0");
+    println!("Clique na janela que deseja fechar...");
 
+    let (conn, screen_num) = RustConnection::connect(None)?;
     let handler = X11Handler::new()?;
-    let windows = handler.list_windows()?;
+    let mut event_handler = EventHandler::new(conn, screen_num);
 
-    println!("Janelas detectadas: {}", windows.len());
-
-    for window in windows.iter().take(5) {
-        if let Ok(name) = handler.get_window_name(*window) {
-            if !name.is_empty() {
-                println!("  - {} (ID: {})", name, window);
-            }
+    if let Some(window) = event_handler.wait_for_window_click()? {
+        if let Ok(name) = handler.get_window_name(window) {
+            println!("Fechando janela: {}", name);
         }
+        handler.kill_window(window)?;
+        println!("Janela fechada com sucesso");
+    } else {
+        println!("Operação cancelada");
     }
 
     Ok(())
